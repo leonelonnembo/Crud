b55df1e946da36f6ccd2a42d6127d108fffc278b
Leonel Onnembo <onnembol@gmail.com>
2023-12-02 03:15:46 -0300

    añadido carrito
    se añadio carrito, aunque no se puede comprar nada muestra los items dentro y el valor total de todos
    
    Co-Authored-By: Leonel Onnembo <110853754+leonelonnembo@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -1,48 +1,98 @@
 // Realizar una solicitud a la nueva URL para obtener los productos
 fetch('https://leonelonne.pythonanywhere.com/productos')
-  .then(response => response.json())
-  .then(data => {
+.then(response => response.json())
+.then(data => {
     const imageContainer = document.getElementById('image-container');
-
+    const carritoContainer = document.getElementById('carrito');
+    
+    // Agrega un nuevo contenedor para la cantidad en el header
+    const carritoCantidadHeader = document.getElementById('carrito-cantidad-header');
+    
+    // Agrega un nuevo contenedor para la ventana emergente
+    const carritoVentana = document.getElementById('carrito-ventana');
+    const carritoContenido = document.getElementById('carrito-contenido');
+    const cerrarCarrito = document.getElementById('cerrar-carrito');
+    
+    // Inicializa el contador del carrito y el valor total
+    let cantidadEnCarrito = 0;
+    let valorTotalCarrito = 0;
+    
+    // Función para agregar un producto al carrito
+    const agregarAlCarrito = (producto) => {
+        // Lógica para agregar el producto al carrito
+        cantidadEnCarrito++;
+        valorTotalCarrito += parseFloat(producto.precio); // Convierte el precio a número
+        
+        carritoCantidadHeader.textContent = cantidadEnCarrito;
+        
+        const carritoItem = document.createElement('div');
+        carritoItem.textContent = producto.nombre + ' - Precio: ' + producto.precio + ' ARS';
+        carritoContenido.appendChild(carritoItem);
+        
+        // Actualiza el valor total en la ventana del carrito
+        actualizarValorTotal();
+    };
+    
+    // Función para actualizar el valor total en la ventana del carrito
+    const actualizarValorTotal = () => {
+        const valorTotalElement = document.getElementById('valor-total-carrito');
+        valorTotalElement.textContent = 'Valor Total: ' + valorTotalCarrito.toFixed(2) + ' ARS';
+    };
+    // Agrega un listener al icono del carrito en el header
+    const carritoIconHeader = document.getElementById('carrito-icon-header');
+    carritoIconHeader.addEventListener('click', () => {
+        // Abre la ventana emergente del carrito al hacer clic en el icono
+        carritoVentana.style.display = 'block';
+    });
+    
+    // Agrega un listener para cerrar la ventana emergente del carrito
+    cerrarCarrito.addEventListener('click', () => {
+        carritoVentana.style.display = 'none';
+    });
+    
     // Itera sobre los datos y crea elementos para los productos
     data.forEach(producto => {
-      const card = document.createElement('div');
-      card.className = 'card';
-      card.style = '--clr: #009688';
-      
-      const imgBox = document.createElement('div');
-      imgBox.className = 'img-box';
-      const img = document.createElement('img');
-      img.src = producto.imagen;
-      img.alt = producto.nombre;
-
-      const content = document.createElement('div');
-      content.className = 'content';
-      
-      const h2 = document.createElement('h2');
-      h2.textContent = producto.nombre;
-      
-      const p = document.createElement('p');
-      p.textContent = `Precio: ${producto.precio} ARS`; // Agrega la moneda según la necesidad
-      
-      const a = document.createElement('a');
-      a.href = "#";
-      a.textContent = 'Sumar al carrito';
-
-      // Organiza los elementos
-      imgBox.appendChild(img);
-      content.appendChild(h2);
-      content.appendChild(p);
-      content.appendChild(a);
-      card.appendChild(imgBox);
-      card.appendChild(content);
-      imageContainer.appendChild(card);
+        const card = document.createElement('div');
+        card.className = 'card';
+        card.style = '--clr: #009688';
+        
+        const imgBox = document.createElement('div');
+        imgBox.className = 'img-box';
+        const img = document.createElement('img');
+        img.src = producto.imagen;
+        img.alt = producto.nombre;
+        
+        const content = document.createElement('div');
+        content.className = 'content';
+        
+        const h2 = document.createElement('h2');
+        h2.textContent = producto.nombre;
+        
+        const p = document.createElement('p');
+        p.textContent = `Precio: ${producto.precio} ARS`;
+        
+        const a = document.createElement('a');
+        a.href = "#";
+        a.textContent = 'Sumar al carrito';
+        
+        // Agrega un listener al botón "Sumar al carrito"
+        a.addEventListener('click', () => {
+            agregarAlCarrito(producto);
+        });
+        
+        // Organiza los elementos
+        imgBox.appendChild(img);
+        content.appendChild(h2);
+        content.appendChild(p);
+        content.appendChild(a);
+        card.appendChild(imgBox);
+        card.appendChild(content);
+        imageContainer.appendChild(card);
     });
-  })
-  .catch(error => {
+})
+.catch(error => {
     console.error('Error al cargar los productos desde la API: ' + error);
-  });
-
+});
 
 
 
@@ -51,7 +101,7 @@ fetch('https://leonelonne.pythonanywhere.com/productos')
 /* fin de carrito */
 
 
-  
+
 //Validacion del form
 
 const $form = document.querySelector('#form');
@@ -60,60 +110,62 @@ $form.addEventListener("submit", handleSubmit);
 
 async function handleSubmit(event) {
     event.preventDefault();
-
+    
     const emailInput = $form.querySelector('input[name="email"]');
     const messageInput = $form.querySelector('textarea[name="message"]');
     const nameInput = $form.querySelector('input[name="name"]');
-
+    
     // Validar correo electrónico con una expresión regular
     const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     
     if (emailInput.value.trim() === '' || 
-        messageInput.value.trim() === '' || 
-        nameInput.value.trim() === '' || 
-        !emailPattern.test(emailInput.value)) {
+    messageInput.value.trim() === '' || 
+    nameInput.value.trim() === '' || 
+    !emailPattern.test(emailInput.value)) {
         // Muestra una notificación de error si los campos están vacíos o el correo no es válido
         Swal.fire(
             'Atención',
             'Por favor, complete todos los campos obligatorios y proporcione un correo electrónico válido.',
             'error'
-        );
-        return; // Detiene el envío del formulario si hay errores
-    }
-
-    // Validar longitud del nombre y mensaje (puedes ajustar los límites según tus necesidades)
-    if (nameInput.value.length < 2 || nameInput.value.length > 50 || 
-        messageInput.value.length < 10 || messageInput.value.length > 500) {
-        Swal.fire(
-            'Atención',
-            'El nombre debe tener entre 2 y 50 caracteres, y el mensaje debe tener entre 10 y 500 caracteres.',
-            'error'
-        );
-        return;
-    }
-
-    // Realiza el envío del formulario si pasa la validación
-    const form = new FormData($form);
-    const response = await fetch($form.action, {
-        method: $form.method,
-        body: form,
-        headers: {
-            'Accept': 'application/json'
+            );
+            return; // Detiene el envío del formulario si hay errores
         }
-    });
-
-    if (response.ok) {
-        $form.reset();
-        Swal.fire(
-            'Gracias por contactarnos.',
-            'Nos comunicaremos contigo pronto.',
-            'success'
-        );
-    } else {
-        Swal.fire(
-            'Error',
-            'Hubo un problema al enviar el formulario.',
-            'error'
-        );
-    }
-}
+        
+        // Validar longitud del nombre y mensaje (puedes ajustar los límites según tus necesidades)
+        if (nameInput.value.length < 2 || nameInput.value.length > 50 || 
+            messageInput.value.length < 10 || messageInput.value.length > 500) {
+                Swal.fire(
+                    'Atención',
+                    'El nombre debe tener entre 2 y 50 caracteres, y el mensaje debe tener entre 10 y 500 caracteres.',
+                    'error'
+                    );
+                    return;
+                }
+                
+                // Realiza el envío del formulario si pasa la validación
+                const form = new FormData($form);
+                const response = await fetch($form.action, {
+                    method: $form.method,
+                    body: form,
+                    headers: {
+                        'Accept': 'application/json'
+                    }
+                });
+                
+                if (response.ok) {
+                    $form.reset();
+                    Swal.fire(
+                        'Gracias por contactarnos.',
+                        'Nos comunicaremos contigo pronto.',
+                        'success'
+                        );
+                    } else {
+                        Swal.fire(
+                            'Error',
+                            'Hubo un problema al enviar el formulario.',
+                            'error'
+                            );
+                        }
+                    }
+                    
+                    
